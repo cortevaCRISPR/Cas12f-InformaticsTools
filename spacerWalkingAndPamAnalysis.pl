2df98761ab731381baf048eaf4f9606730207010
cortevaCRISPR <62144486+cortevaCRISPR@users.noreply.github.com>
2020-08-14 10:29:18 -0500

    Update spacerWalkingAndPamAnalysis.pl

--- spacerWalkingAndPamAnalysis.pl
+++ spacerWalkingAndPamAnalysis.pl
@@ -1,47 +1,49 @@
 #!/usr/bin/perl
-##This perl program captures and counts the adaptor ligated sequences at every protospacer position (Spacer Walking) and also the PAM 
-##regions between the 5'flank and 3'flank (PAM Analysis)
+use File::Basename;
 
-##Usage: perl spacerWalkingAndPamAnalysis Experiment.txt 
+## This perl program captures and counts the adaptor ligated sequences at every protospacer position (Spacer Walking) and also the PAM 
+## regions between the 5'flank and 3'flank (PAM Analysis)
 
-##In the Spacer Walking mode, the Experiment.txt file(tab delimited) will contain all the protospacer positions and the associated flanks
-##Example
-## LibraryName	Position	Barcode	5_Flank	3_Flank
-## Cas9	1	CTAGGT	CGGCATTCCTGCTGAACCGCTCTTCCGATCTA	AGTTGACCCA
-## Cas9	2	CTAGGT	CGGCATTCCTGCTGAACCGCTCTTCCGATCTCA	AGTTGACCCA
-## Cas9	3	CTAGGT	CGGCATTCCTGCTGAACCGCTCTTCCGATCTACA	AGTTGACCCA
+## Usage: perl spacerWalkingAndPamAnalysis RESULTS_DIR INPUT_FILE.txt EXPT.fastq BARCODE_LEN PAM_LEN
+## Example: perl spacerWalkingAndPamAnalysis /path/to/resultsdir input.txt AK101.fastq 6 7
+## ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-##In the PAM Analysis mode, the Experiment.txt file will contain just the 1 position (highest activity) and its associated flanks 
-##Example
-## LibraryName	Position	Barcode	5_Flank	3_Flank
-## Cas9	1	CTAGGT	CGGCATTCCTGCTGAACCGCTCTTCCGATCTACA	AGTTGACCCA
+## Input Parameter values
 
-----------------------------------------------------------------------------------------------------------
+my $dir = $ARGV[0];
+my $inputFile = $ARGV[1];
+my $fastq =$ARGV[2];
+my $name = basename($fastq);
+my $exp = $name;
+$exp =~ s/.fastq//g;
+my $barcodeLen=$ARGV[3];
+my $pamLen=$ARGV[4];
 
-##Define parameter values
-my $inputFile = $ARGV[0];
-my $barcodeLen=6;
-my $pamLen=7;
-my $bounds="both_flanks";
-my $exp = "AK4130001";
 my %flank_h={};
-my $tsvdir = "TSV/";
+my $tsvdir = "$dir/PAM_Data/";
+
 mkdir $tsvdir;
+
+## Read the input file and store values in a hash
 
 open(F,$inputFile) || die "cannot open file"; 
 while(<F>){
 chomp;
 my @arr = split("\t",$_);
+unless (/^Name/){
 $flank_h{$arr[2]}{$arr[1]}{"name"}=$arr[0];
 $flank_h{$arr[2]}{$arr[1]}{"5_flank"}=$arr[3];
 $flank_h{$arr[2]}{$arr[1]}{"3_flank"}=$arr[4];
 }
+}
 close(F);
+
 my @keys = keys %flank_h;
 my @pos = sort keys %{ $flank_h{$keys[0]} };
 
-##Loop over each position and count the number of PAM sequences between the 5_flank and 3_flank from the fastq file 
- foreach my $p(@pos){
+## Loop over each position and count the number of PAM sequences between the 5_flank and 3_flank from the fastq file 
+
+foreach my $p(@pos){
 	my $fname = $tsvdir.$exp."_".$p."_pam_expt.tsv"; 
 	my $sname = $tsvdir.$exp."_".$p.".summary.txt"; 
 	my $header = $exp."-".$p;
@@ -50,7 +52,7 @@ my @pos = sort keys %{ $flank_h{$keys[0]} };
 
 	open(FILE,">$fname");
 
-	open(IN,"$exp.fastq") || die "cannot open file";
+	open(IN,"$fastq") || die "cannot open file";
 	$GET=0;
 	while(<IN>){
 		if(/^\@/o){$GET=1;next;} 
@@ -61,10 +63,7 @@ my @pos = sort keys %{ $flank_h{$keys[0]} };
 				$up=$flank_h{$bc}{$p}{"5_flank"};
 				$dn=$flank_h{$bc}{$p}{"3_flank"};
 				$MATCH = undef;
-				if( $bounds eq "both_flanks" ) { if(/$up([ACGT]{$pamLen})$dn/){ $MATCH = $1;}} 
-				elsif( $bounds eq "dn_flank" ) { if(/([ACGT]{$pamLen})$dn/){ $MATCH = $1;}}
-				elsif( $bounds eq "up_flank" ) { if(/$up([ACGT]{$pamLen})/){ $MATCH = $1;}}
-				elsif( $bounds eq "variable" ) { if(/$up([ACGT]+)$dn/){ $MATCH = $1; }}
+				if(/$up([ACGT]{$pamLen})$dn/){ $MATCH = $1;}
 				if( defined( $MATCH ) ) {
 					$ctH{$bc}{$MATCH}+=1;
 					$mct++;
@@ -73,26 +72,24 @@ my @pos = sort keys %{ $flank_h{$keys[0]} };
 		}
 		$GET=0;
 	}
+
 	## Write the count file 
+
 	foreach my $bc (sort (keys %ctH) ) {
-		print FILE "#Expt\tSite\tCount\n";
+		print FILE "#Sample\tPAM\tCount\n";
 		foreach $k (sort { $ctH{$bc}{$b}<=>$ctH{$bc}{$a} } (keys %{$ctH{$bc}}) ) {
 			print FILE "$flank_h{$bc}{$p}{\"name\"}\t$k\t$ctH{$bc}{$k}\n";
 		}
-		print FILE "\nTop 50 rejected reads for $flank_h{$bc}{$p}{\"name\"}\n";
-		$c=0;
-		foreach $r (sort {$skpH{$bc}{$b}<=>$skpH{$bc}{$a}} (keys %{$skpH{$bc}})){
-			print FILE "$r\t$skpH{$bc}{$r}\n";
-			if($c>48){print FILE "\n";last;}
-				$c++;
-		}
+		print FILE "\n\n";
 	}
 
 	close(FILE);
 	
 	## Write the summary file 
+
 	open(S, ">$sname");
-	print S "# $header $bounds\n# Total Reads:\t\t$tr\n# Barcode Matched:\t$bct\n# PAM Counted:\t\t$mct\n"; 
+	print S "# $header\n# Total Reads:\t\t$tr\n# Barcode Matched:\t$bct\n# PAM Counted:\t\t$mct\n"; 
 	close(S);
 
 }
+
